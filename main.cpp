2ca49d4464ec03bf5b783c0e0a802ebc4b347441
tanneradkins1234 <tanneradkins1234@gmail.com>
2025-12-05 16:29:54 -0500

    Create Tilemap

--- main.cpp
+++ main.cpp
@@ -1,39 +1,47 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Hello World");
-    sf::CircleShape circle(20);
-    circle.setPosition(980, 540);
-    circle.setOrigin(10, 10);
-    
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Hello World");
+    vector<vector<int>> vec(6, vector<int>(10, 0));
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec[i].size(); j++)
+        {
+            cout << vec[i][j] << " ";
+        }
+        cout << endl;
+    }
     sf::Clock clock;
     float speed = 500.0f;
     while (window.isOpen()) 
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-            if (event.type == 
-            sf::Event::Closed)
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
                 window.close();
+            }
+        }
+
         float deltaTime = clock.restart().asSeconds();
+
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::W))
         {
-            circle.move(0, -speed * deltaTime);
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::S))
         {
-            circle.move(0, speed * deltaTime);
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::A))
         {
-            circle.move(-speed * deltaTime, 0);
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::D))
         {
-            circle.move(speed * deltaTime, 0);
         }
         window.clear();
-        window.draw(circle);
         window.display();
     }
     return 0;
