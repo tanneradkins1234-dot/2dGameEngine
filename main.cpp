e408ce4d45522034da0338d75113ffe0871a0cd8
tanneradkins1234-dot <tanneradkins1234@gmail.com>
2025-12-06 00:23:24 -0500

    Add Perlin Noise

--- main.cpp
+++ main.cpp
@@ -1,20 +1,93 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <map>
 #include <vector>
+#include <SFML/Graphics/Vertex.hpp>
+
 using namespace std;
-
-int main() {
-    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Hello World");
-    vector<vector<int>> vec(6, vector<int>(10, 0));
-
-    for (int i = 0; i < vec.size(); i++)
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+    bool load(const std::filesystem::path& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
     {
-        for (int j = 0; j < vec[i].size(); j++)
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::PrimitiveType::Triangles);
+        m_vertices.resize(width * height * 6);
+
+        // populate the vertex array, with two triangles per tile
+        for (unsigned int i = 0; i < width; ++i)
         {
-            cout << vec[i][j] << " ";
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // get the current tile number
+                const int tileNumber = tiles[i + j * width];
+
+                // find its position in the tileset texture
+                const int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                const int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+                // get a pointer to the triangles' vertices of the current tile
+                sf::Vertex* triangles = &m_vertices[(i + j * width) * 6];
+
+                // define the 6 corners of the two triangles
+                triangles[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                triangles[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                triangles[2].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+                triangles[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+                triangles[4].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                triangles[5].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+
+                // define the 6 matching texture coordinates
+                triangles[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                triangles[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                triangles[2].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+                triangles[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+                triangles[4].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                triangles[5].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+            }
         }
-        cout << endl;
+
+        return true;
     }
+
+private:
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture     m_tileset;
+};
+int main() {
+    sf::RenderWindow window(sf::VideoMode({512, 256}), "Hello World");
+
+    constexpr std::array level = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 3, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    };
+
+    TileMap map;
+       if (!map.load("Universe1.png", {32, 32}, level.data(), 16, 8))
+            return -1;
+
     sf::Clock clock;
     float speed = 500.0f;
     while (window.isOpen()) 
@@ -42,6 +115,7 @@ int main() {
         {
         }
         window.clear();
+        window.draw(map);
         window.display();
     }
     return 0;
